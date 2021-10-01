861478765a3bc21591ebb033b065bc278457d993
Поддубко Сергей <snpod@rambler.ru>
2021-10-01 23:00:20 +1000

    posts_and_scripts.v1.1

--- forum/forum/settings.py
+++ forum/forum/settings.py
@@ -20,7 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'v02n!4t7=rlx@zim3+ry#7z=v7dmpezrdwk729m4i)bx_$-_w@'
+
+
+with open('D:\\Dev\\posts_and_scripts\\venv\\secret_key') as f:
+    SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
