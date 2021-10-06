66584d47204fd50601b32928b9c7485263399def
Поддубко Сергей <snpod@rambler.ru>
2021-10-06 11:17:07 +1000

    posts and scripts.v2

--- forum/blog/views.py
+++ forum/blog/views.py
@@ -5,9 +5,9 @@ from django.core.paginator import Paginator
 from .models import User
 from .forms import MyscriptsForm, CommentForm
 from django.shortcuts import redirect
-from forum.settings import number_of_elements_in_page
+from forum.settings import EMAIL_HOST_USER
 from django.views import generic
-
+from django.core.mail import send_mass_mail
 
 
 # def index(request):
@@ -49,6 +49,7 @@ def script_detail(request, slug):
                'comments': comments, }
     return render(request, 'blog/script_detail.html', context)
 
+
 @login_required
 def myscript_edit(request, slug):
     myscript = get_object_or_404(MyScripts, slug=slug)
@@ -58,7 +59,8 @@ def myscript_edit(request, slug):
         form = MyscriptsForm(instance=myscript)
         return render(request, 'blog/create_post.html',
                                {'form': form, 'is_edit': True})
-    form = MyscriptsForm(request.POST, files=request.FILES or None, instance=myscript)
+    form = MyscriptsForm(request.POST, files=request.FILES or None,
+                         instance=myscript)
     if not form.is_valid():
         return render(request, 'blog/create_post.html',
                                {'form': form, 'is_edit': True})
@@ -75,4 +77,33 @@ def add_comment(request, slug):
         comment.author = request.user
         comment.myscripts = post
         comment.save()
+        list_of_recipients = [recipient.author.email for recipient in
+                              post.comments_blog.select_related('author').
+                              all()]
+        author_email = post.author.email
+        list_of_recipients.append(author_email)
+        emails = list(set(list_of_recipients))
+        subject = 'Новый комментарий'
+        messages = []
+        for email in emails:
+            if email == request.user.email:
+                continue
+            if email == author_email:
+                message_for_author = ('Привет! На твой пост пришел '
+                                      'новый комментарий от пользователя '
+                                      f'{request.user}.')
+                message = (subject, message_for_author, EMAIL_HOST_USER,
+                           [author_email, ])
+                messages.append(message)
+            else:
+                full_name = f'{post.author.first_name} {post.author.last_name}'
+                message_for_other = (f'Привет! На пост "{post}........" '
+                                     f'пользователя {full_name} пришел '
+                                     'новый комментарий от '
+                                     f'пользователя {request.user}.')
+                message = (subject, message_for_other, EMAIL_HOST_USER,
+                           [email, ])
+                messages.append(message)
+        if len(messages) > 0:
+            send_mass_mail((messages), fail_silently=False)
     return redirect('blog:script_detail', slug=slug)
