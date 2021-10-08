a2ec2c9eac52df66edbfc56466aeb796d658a50d
Поддубко Сергей <snpod@rambler.ru>
2021-10-08 11:50:19 +1000

    posts and scripts.v3

--- forum/home/views.py
+++ forum/home/views.py
@@ -143,10 +143,18 @@ def add_comment(request, post_id):
 @login_required
 def follow_index(request):
     post_list = Post.objects.filter(author__following__user=request.user)
+    users = [(user, user.posts.count()) for user in
+             User.objects.all() if user.posts.count() > 0]
+    sort_user_list = reversed(sorted(users, key=itemgetter(1)))
+    group_list = Group.objects.all()
+    rev_post_list = list(reversed(post_list))
+    rev_post_list = list(reversed(rev_post_list[:15]))
+    
     paginator = Paginator(post_list, number_of_elements_in_page)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
-    context = {'page_obj': page_obj}
+    context = {'page_obj': page_obj, 'post_list': rev_post_list,
+               'group_list': group_list, 'sort_user_list': sort_user_list}
     return render(request, 'home/follow.html', context)
 
 
