fd20439899ab122700a579f0d20ceb902d647539
Поддубко Сергей <snpod@rambler.ru>
2021-10-09 15:57:41 +1000

    posts and scripts.v3.1

--- forum/home/urls.py
+++ forum/home/urls.py
@@ -3,6 +3,7 @@ from . import views
 app_name = 'home'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('posts_commented_of_author/<author_comment>/', views.index, name='index'),
     path('group/<slug:slug>/', views.group_posts, name='group_list'),
     path('profile/<str:username>/', views.profile, name='profile'),
     path('posts/<int:post_id>/', views.post_detail, name='post_detail'),
--- forum/home/views.py
+++ forum/home/views.py
@@ -11,11 +11,20 @@ from django.core.mail import send_mass_mail
 from operator import itemgetter
 
 
-def index(request):
-    post_list = Post.objects.all()
+def index(request, author_comment=None):
+    if author_comment:
+        comments_list = Comment.objects.select_related('post').filter(
+                        author__username=author_comment).all()
+        post_list = list(set([el.post for el in comments_list]))
+    else:
+        post_list = Post.objects.all()
     users = [(user, user.posts.count()) for user in
              User.objects.all() if user.posts.count() > 0]
     sort_user_list = reversed(sorted(users, key=itemgetter(1)))
+    author_comments = [(user, user.comments.count()) for user in
+                       User.objects.all() if user.comments.count() > 0]
+    sort_author_comments_list = reversed(sorted(author_comments,
+                                         key=itemgetter(1)))
     group_list = Group.objects.all()
     rev_post_list = list(reversed(post_list))
     rev_post_list = list(reversed(rev_post_list[:15]))
@@ -23,7 +32,10 @@ def index(request):
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
     context = {'page_obj': page_obj, 'post_list': rev_post_list,
-               'group_list': group_list, 'sort_user_list': sort_user_list}
+               'group_list': group_list, 'sort_user_list': sort_user_list,
+               'sort_author_comments_list': sort_author_comments_list}
+    if author_comment:
+        return render(request, 'home/index_of_comments.html', context)
     return render(request, 'home/index.html', context)
 
 
